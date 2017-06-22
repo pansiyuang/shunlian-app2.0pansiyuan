d624e9679291c6989374633fa501ac81c7df7fad
zhanghe <zhanghe@shunliandongli.com>
2017-06-22 17:49:56 +0800

    zh 基类整理

--- app/src/main/java/com/shunlian/app/ui/MainActivity.java
+++ app/src/main/java/com/shunlian/app/ui/MainActivity.java
@@ -1,10 +1,18 @@
 package com.shunlian.app.ui;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
 import com.shunlian.app.R;
 import com.shunlian.app.presenter.TestPresenter;
 
 public class MainActivity extends BaseActivity {
 
+
+    private long mExitTime;
+    private boolean isDoubleBack = false;
 
     /**
      * 布局id
@@ -22,7 +30,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData() {
 
-        TestPresenter testPresenter = new TestPresenter();
+        TestPresenter testPresenter = new TestPresenter(this,null);
 
 
     }
@@ -31,6 +39,27 @@ public class MainActivity extends BaseActivity {
     protected void initListener() {
         super.initListener();
 
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if ((System.currentTimeMillis() - mExitTime) > 2000) {
+                Toast.makeText(this, "再按一次退出顺联动力", Toast.LENGTH_SHORT).show();
+                mExitTime = System.currentTimeMillis();
+            } else {
+                isDoubleBack = true;
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                try {
+                    startActivity(intent);
+                }catch (ActivityNotFoundException e){
+                    finish();
+                }
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
