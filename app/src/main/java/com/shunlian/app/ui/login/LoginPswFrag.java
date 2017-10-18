09c64fc29e78df519b657378b5fed19af083ef4c
wuxiaojian <541223016@qq.com>
2017-10-18 14:06:50 +0800

    密码显示隐藏逻辑

--- app/src/main/java/com/shunlian/app/ui/login/LoginPswFrag.java
+++ app/src/main/java/com/shunlian/app/ui/login/LoginPswFrag.java
@@ -1,6 +1,8 @@
 package com.shunlian.app.ui.login;
 
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,24 +38,51 @@ public class LoginPswFrag extends BaseFragment implements View.OnClickListener {
         iv_hidden_psw = (MyImageView) rootView.findViewById(R.id.iv_hidden_psw);
         edt_psw = (EditText) rootView.findViewById(R.id.edt_psw);
         iv_hidden_psw.setOnClickListener(this);
+        edt_psw.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (edt_psw.getText().toString().isEmpty()) {
+                    iv_hidden_psw.setVisibility(View.INVISIBLE);
+                    hiddenPsw(false);
+                } else {
+                    iv_hidden_psw.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_hidden_psw:
-                if (isHidden) {
-                    edt_psw.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-                    isHidden = false;
-                } else {
-                    edt_psw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-                    isHidden = true;
-                }
+                hiddenPsw(isHidden);
                 int index = edt_psw.getText().toString().length();
                 if (index > 0) {
                     edt_psw.setSelection(index);
                 }
                 break;
+        }
+    }
+
+    public void hiddenPsw(boolean b) {
+        if (b) {
+            edt_psw.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            iv_hidden_psw.setImageResource(R.mipmap.icon_login_eyes_n);
+            isHidden = false;
+        } else {
+            edt_psw.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            iv_hidden_psw.setImageResource(R.mipmap.icon_login_eyes_h);
+            isHidden = true;
         }
     }
 }
