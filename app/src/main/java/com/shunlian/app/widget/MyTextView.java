93554fdc47c23f075d11fa9557fb621c84e19d94
zhanghe <zhanghe@shunliandongli.com>
2017-10-30 09:36:10 +0800

    项目中以后使用MyTextView

---
+++ app/src/main/java/com/shunlian/app/widget/MyTextView.java
@@ -0,0 +1,23 @@
+package com.shunlian.app.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/10/30.
+ */
+
+public class MyTextView extends TextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
