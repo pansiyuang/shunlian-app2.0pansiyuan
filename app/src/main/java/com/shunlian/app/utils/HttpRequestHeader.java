b2979d1c738297da1dcc724f3efd3e063f996cff
zhanghe <zhanghe@shunliandongli.com>
2017-11-20 09:33:27 +0800

    添加token

--- app/src/main/java/com/shunlian/app/utils/HttpRequestHeader.java
+++ app/src/main/java/com/shunlian/app/utils/HttpRequestHeader.java
@@ -49,7 +49,8 @@ public class HttpRequestHeader implements Interceptor {
                 .addHeader("X-Device-ID", SharedPrefUtil.getSharedPrfString("X-Device-ID", "744D9FC3-5DBD-3EDD-A589-56D77BDB0E5D"))
                 .addHeader("resolution", SharedPrefUtil.getSharedPrfString("resolution", "720x1184"))
                 .addHeader("Net-Type", SharedPrefUtil.getSharedPrfString("Net-Type", "OTHER"))//网络类型
-                .addHeader("X-Client-Ip", SharedPrefUtil.getSharedPrfString("DeviceIp", "192.168.1.1"));
+                .addHeader("X-Client-Ip", SharedPrefUtil.getSharedPrfString("DeviceIp", "192.168.1.1"))
+                .addHeader("token", SharedPrefUtil.getSharedPrfString("token", ""));
 //                .addHeader("Content-Type", "application/json");
         if (isRemoveContentType) {
             builder.removeHeader("Content-Type");
