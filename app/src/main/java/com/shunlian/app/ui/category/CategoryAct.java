3dc27c14a5e904be3d9b556ea93aedc8539617c1
wuxiaojian <541223016@qq.com>
2018-01-05 13:53:59 +0800

    修改购物车选择优惠dialog逻辑

--- app/src/main/java/com/shunlian/app/ui/category/CategoryAct.java
+++ app/src/main/java/com/shunlian/app/ui/category/CategoryAct.java
@@ -1,13 +1,21 @@
 package com.shunlian.app.ui.category;
 
+import android.support.v7.widget.RecyclerView;
+
 import com.shunlian.app.R;
 import com.shunlian.app.ui.SideslipBaseActivity;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2018/1/2.
  */
 
 public class CategoryAct extends SideslipBaseActivity {
+
+    @BindView(R.id.recycle_category)
+    RecyclerView recycle_category;
+
     /**
      * 布局id
      *
