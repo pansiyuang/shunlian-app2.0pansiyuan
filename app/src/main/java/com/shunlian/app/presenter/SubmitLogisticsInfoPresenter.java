5c9ce388d60c873062869b0e60d40a47c8b831a8
wuxiaojian <541223016@qq.com>
2018-01-11 15:04:49 +0800

    上传图片功能压缩

--- app/src/main/java/com/shunlian/app/presenter/SubmitLogisticsInfoPresenter.java
+++ app/src/main/java/com/shunlian/app/presenter/SubmitLogisticsInfoPresenter.java
@@ -109,7 +109,7 @@ public class SubmitLogisticsInfoPresenter extends BasePresenter<ISubmitLogistics
     public void uploadPic(List<ImageEntity> filePath, final String uploadPath) {
         final Map<String, RequestBody> params = new HashMap<>();
         for (int i = 0; i < filePath.size(); i++) {
-            File file = new File(filePath.get(i).imgPath);
+            File file = filePath.get(i).file;
             RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
             LogUtil.httpLogW("file:" + file.getName());
             UploadFileRequestBody uploadFileRequestBody = new UploadFileRequestBody(requestBody, new ProgressListener() {
@@ -123,7 +123,7 @@ public class SubmitLogisticsInfoPresenter extends BasePresenter<ISubmitLogistics
                 public void onDetailProgress(long written, long total, String tag) {
 
                 }
-            }, filePath.get(i).imgPath);
+            },file.getAbsolutePath());
             params.put("file[]\"; filename=\"" + file.getName(), uploadFileRequestBody);
         }
         RequestBody body = RequestBody.create(okhttp3.MediaType.parse("text/plain"), uploadPath);
