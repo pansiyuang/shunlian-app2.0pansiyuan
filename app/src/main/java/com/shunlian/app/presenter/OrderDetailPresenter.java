7cb622d7fefa42e72b98b9dbb1e963838d39af39
yangxiaofeng <yangxiaofeng@shunliandongli.com>
2018-01-17 14:35:08 +0800

    订单详情优化

--- app/src/main/java/com/shunlian/app/presenter/OrderDetailPresenter.java
+++ app/src/main/java/com/shunlian/app/presenter/OrderDetailPresenter.java
@@ -6,6 +6,7 @@ import com.shunlian.app.bean.BaseEntity;
 import com.shunlian.app.bean.CommonEntity;
 import com.shunlian.app.bean.OrderdetailEntity;
 import com.shunlian.app.listener.SimpleNetDataCallback;
+import com.shunlian.app.ui.confirm_order.SearchOrderResultActivity;
 import com.shunlian.app.ui.order.AllFrag;
 import com.shunlian.app.utils.Common;
 import com.shunlian.app.view.OrderdetailView;
@@ -75,7 +76,8 @@ public class OrderDetailPresenter extends BasePresenter<OrderdetailView> {
                     Common.staticToast(data.message);
                 }
                 AllFrag.isRefreshItem = true;
-                initApi();
+                SearchOrderResultActivity.isRefreshItem = true;
+                initApiData();
             }
         });
     }
@@ -118,7 +120,9 @@ public class OrderDetailPresenter extends BasePresenter<OrderdetailView> {
                 if (data != null){
                     Common.staticToast(data.message);
                 }
-                initApi();
+                AllFrag.isRefreshItem = true;
+                SearchOrderResultActivity.isRefreshItem = true;
+                initApiData();
             }
         });
     }
@@ -141,7 +145,8 @@ public class OrderDetailPresenter extends BasePresenter<OrderdetailView> {
                     Common.staticToast(data.message);
                 }
                 AllFrag.isRefreshItem = true;
-                initApi();
+                SearchOrderResultActivity.isRefreshItem = true;
+                initApiData();
             }
         });
     }
