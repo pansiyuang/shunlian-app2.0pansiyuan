98f6357feb5f9e4cb7dc9f0c4f54b40fd8ef69f0
zhanghe <zhanghe@shunliandongli.com>
2018-03-23 17:26:22 +0800

    提前关注，评论列表，评论详情文字

--- app/src/main/java/com/shunlian/app/presenter/FindCommentListPresenter.java
+++ app/src/main/java/com/shunlian/app/presenter/FindCommentListPresenter.java
@@ -69,7 +69,8 @@ public class FindCommentListPresenter extends FindCommentPresenter<IFindCommentL
         map.put("article_id", mArticle_id);
         sortAndMD5(map);
         Call<BaseEntity<FindCommentListEntity>> baseEntityCall = getApiService().findcommentList(map);
-        getNetData(0, failureCode, isShow, baseEntityCall, new SimpleNetDataCallback<BaseEntity<FindCommentListEntity>>() {
+        getNetData(0, failureCode, isShow, baseEntityCall,
+                new SimpleNetDataCallback<BaseEntity<FindCommentListEntity>>() {
             @Override
             public void onSuccess(BaseEntity<FindCommentListEntity> entity) {
                 super.onSuccess(entity);
@@ -106,7 +107,8 @@ public class FindCommentListPresenter extends FindCommentPresenter<IFindCommentL
 
     private void setCommentList(int currentPage, int allPage) {
         if (adapter == null) {
-            adapter = new FindCommentListAdapter(context, mItemComments, hotCommentCount,comment_type);
+            adapter = new FindCommentListAdapter(context, mItemComments,
+                    hotCommentCount,comment_type);
             iView.setAdapter(adapter);
             adapter.setOnReloadListener(new BaseRecyclerAdapter.OnReloadListener() {
                 @Override
@@ -199,7 +201,8 @@ public class FindCommentListPresenter extends FindCommentPresenter<IFindCommentL
             Common.staticToasts(context, message, R.mipmap.icon_common_duihao);
             return;
         }
-        Common.staticToasts(context, "发布成功", R.mipmap.icon_common_duihao);
+        Common.staticToasts(context, getStringResouce(R.string.send_success),
+                R.mipmap.icon_common_duihao);
         if (currentTouchItem != -1) {
             mItemComments.remove(currentTouchItem);
             mItemComments.add(currentTouchItem, insert_item);
