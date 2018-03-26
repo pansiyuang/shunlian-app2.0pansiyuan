4644a3e681bc96fdb094ce571aa372dc34710aa3
zhanghe <zhanghe@shunliandongli.com>
2018-03-26 11:28:35 +0800

    拉取代码

--- app/src/main/java/com/shunlian/app/presenter/ExperienceDetailPresenter.java
+++ app/src/main/java/com/shunlian/app/presenter/ExperienceDetailPresenter.java
@@ -2,7 +2,15 @@ package com.shunlian.app.presenter;
 
 import android.content.Context;
 
+import com.shunlian.app.bean.BaseEntity;
+import com.shunlian.app.bean.EmptyEntity;
+import com.shunlian.app.listener.SimpleNetDataCallback;
 import com.shunlian.app.view.IExperienceDetailView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
 
 /**
  * Created by Administrator on 2018/3/26.
@@ -11,11 +19,13 @@ import com.shunlian.app.view.IExperienceDetailView;
 public class ExperienceDetailPresenter extends BasePresenter<IExperienceDetailView> {
 
     private String mExperience_id;
+    private final String page_size = "10";
 
     public ExperienceDetailPresenter(Context context, IExperienceDetailView iView,
                                      String experience_id) {
         super(context, iView);
         mExperience_id = experience_id;
+        initApi();
     }
 
     /**
@@ -39,6 +49,19 @@ public class ExperienceDetailPresenter extends BasePresenter<IExperienceDetailVi
      */
     @Override
     public void initApi() {
+        Map<String,String> map = new HashMap<>();
+        map.put("experience_id",mExperience_id);
+        map.put("page",String.valueOf(currentPage));
+        map.put("page_size",page_size);
+        sortAndMD5(map);
 
+        Call<BaseEntity<EmptyEntity>> baseEntityCall = getApiService().experienceDetail(map);
+        getNetData(true,baseEntityCall,new SimpleNetDataCallback<BaseEntity<EmptyEntity>>(){
+            @Override
+            public void onSuccess(BaseEntity<EmptyEntity> entity) {
+                super.onSuccess(entity);
+
+            }
+        });
     }
 }
