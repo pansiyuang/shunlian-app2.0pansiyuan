19a11bfe95ce34a59732dd904ea73e1c2ad0201c
wuxiaojian <541223016@qq.com>
2018-03-29 09:08:27 +0800

    修改精选

--- app/src/main/java/com/shunlian/app/adapter/ArticleAdapter.java
+++ app/src/main/java/com/shunlian/app/adapter/ArticleAdapter.java
@@ -256,7 +256,6 @@ public class ArticleAdapter extends BaseRecyclerAdapter<ArticleEntity.Article> {
             recycler_tags.setLayoutManager(manager);
             recycler_tags.setNestedScrollingEnabled(false);
         }
-
     }
 
     public class ArticleViewHolder extends BaseRecyclerViewHolder implements View.OnClickListener {
