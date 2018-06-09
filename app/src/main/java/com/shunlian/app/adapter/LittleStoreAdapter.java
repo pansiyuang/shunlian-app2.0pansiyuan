fdd1c37e17924059adda57df826fcd4268a19940
wuxiaojian <541223016@qq.com>
2018-06-09 14:38:30 +0800

    修改我的小店

--- app/src/main/java/com/shunlian/app/adapter/LittleStoreAdapter.java
+++ app/src/main/java/com/shunlian/app/adapter/LittleStoreAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.shunlian.app.R;
 import com.shunlian.app.bean.GoodsDeatilEntity;
+import com.shunlian.app.newchat.entity.ChatGoodsEntity;
 import com.shunlian.app.utils.DeviceInfoUtil;
 import com.shunlian.app.utils.GlideUtils;
 import com.shunlian.app.utils.TransformUtil;
@@ -47,7 +48,7 @@ public class LittleStoreAdapter extends BaseRecyclerAdapter<GoodsDeatilEntity.Go
             LittleStoreHolderView littleHolderView = (LittleStoreHolderView) holder;
             GoodsDeatilEntity.Goods goods = lists.get(position);
 
-            GlideUtils.getInstance().loadImage(context, littleHolderView.miv_icon, goods.thumb,false);
+            GlideUtils.getInstance().loadImage(context, littleHolderView.miv_icon, goods.thumb, false);
 
             littleHolderView.tv_goods_title.setText(goods.title);
             if (!isEmpty(goods.price)) {
