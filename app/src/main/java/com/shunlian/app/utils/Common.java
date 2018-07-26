61fd44e9c3da8204b241cb39aaf6c50eff8a98f9
zhanghe <zhanghe@shunliandongli.com>
2018-07-26 20:19:23 +0800

    优化

--- app/src/main/java/com/shunlian/app/utils/Common.java
+++ app/src/main/java/com/shunlian/app/utils/Common.java
@@ -386,11 +386,13 @@ public class Common {
                 UserCouponListAct.startAct(context,params[0]);
                 break;
             case "discountgoods":
-                String id = "";
-                if (params.length >= 2){
-                    id = params[1];
+                if (params != null && params.length > 0){
+                    String id = "";
+                    if (params != null && params.length > 1){
+                        id = params[1];
+                    }
+                    CouponGoodsAct.startAct(context,params[0],id);
                 }
-                CouponGoodsAct.startAct(context,params[0],id);
                 break;
             case "chat"://聊天
 
