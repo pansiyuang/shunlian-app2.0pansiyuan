c21385f18ad645c3532d4fb1a79a23a04ae8e54b
wuxiaojian <541223016@qq.com>
2018-07-30 15:40:54 +0800

    修改文章点赞

--- app/src/main/java/com/shunlian/app/adapter/ArticleAdapter.java
+++ app/src/main/java/com/shunlian/app/adapter/ArticleAdapter.java
@@ -1,6 +1,7 @@
 package com.shunlian.app.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.SpannableStringBuilder;
@@ -101,6 +102,23 @@ public class ArticleAdapter extends BaseRecyclerAdapter<ArticleEntity.Article> {
                 tagViewHolder.itemView.setLayoutParams(layoutParams);
             }
         }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position, List payloads) {
+        if (!isEmpty(payloads) && holder instanceof ArticleViewHolder) {
+            ArticleViewHolder articleViewHolder = (ArticleViewHolder) holder;
+            ArticleEntity.Article article = (ArticleEntity.Article) payloads.get(0);
+
+            articleViewHolder.tv_share_count.setText(article.forwards);
+            articleViewHolder.tv_evaluate_count.setText(article.likes);
+            if ("0".equals(article.had_like)) {
+                articleViewHolder.miv_evaluate.setImageResource(R.mipmap.icon_found_zan_n);
+            } else {
+                articleViewHolder.miv_evaluate.setImageResource(R.mipmap.icon_found_zan_h);
+            }
+        }
+        super.onBindViewHolder(holder, position, payloads);
     }
 
     public void handlerItem(RecyclerView.ViewHolder holder, final int position) {
@@ -220,7 +238,7 @@ public class ArticleAdapter extends BaseRecyclerAdapter<ArticleEntity.Article> {
         for (int i = 0; i < lists.size(); i++) {
             if (articleId.equals(lists.get(i).id)) {
                 lists.get(i).had_like = hadLike;
-                notifyItemChanged(i + 1);
+                notifyItemChanged(i + 1, lists.get(i));
                 break;
             }
         }
