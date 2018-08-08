a6c1fdf0e1e682bca0841850d33d1913f561f536
zhanghe <zhanghe@shunliandongli.com>
2018-08-08 16:56:49 +0800

    微信分享返回app优化，商品详情去掉几个人想要

--- app/src/main/java/com/shunlian/app/adapter/GoodsDetailAdapter.java
+++ app/src/main/java/com/shunlian/app/adapter/GoodsDetailAdapter.java
@@ -605,13 +605,13 @@ public class GoodsDetailAdapter extends BaseRecyclerAdapter<String> implements P
             GoodsDeatilEntity.GoodsData goods_data = mGoodsEntity.goods_data;
             if (goods_data != null) {
                 mHolder.mtv_sales.setText(isEmpty(goods_data.sales_desc)?"":goods_data.sales_desc);
-                int stock = Integer.parseInt(isEmpty(mGoodsEntity.stock)?"0":mGoodsEntity.stock);
+                /*int stock = Integer.parseInt(isEmpty(mGoodsEntity.stock)?"0":mGoodsEntity.stock);
                 if ("0".equals(mGoodsEntity.status) || stock <= 0){//商品下架或者库存为0显示几个人还想要
                     visible(mHolder.mtv_want);
                     mHolder.mtv_want.setText(goods_data.want_num+"人还想要");
                 }else {
                     gone(mHolder.mtv_want);
-                }
+                }*/
             }
             mHolder.mtv_address.setText(mGoodsEntity.area);
 
