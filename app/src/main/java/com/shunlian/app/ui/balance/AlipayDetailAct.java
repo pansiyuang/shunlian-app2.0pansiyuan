de77a073da064afb477b7a425ee21841d820964a
yangxiaofeng <yangxiaofeng@shunliandongli.com>
2018-10-12 09:34:46 +0800

    提现优化

--- app/src/main/java/com/shunlian/app/ui/balance/AlipayDetailAct.java
+++ app/src/main/java/com/shunlian/app/ui/balance/AlipayDetailAct.java
@@ -1,19 +1,29 @@
 package com.shunlian.app.ui.balance;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.shunlian.app.R;
 import com.shunlian.app.adapter.AlipayDetailAdapter;
 import com.shunlian.app.bean.GetRealInfoEntity;
+import com.shunlian.app.bean.PersonalcenterEntity;
 import com.shunlian.app.presenter.PAlipayDetail;
 import com.shunlian.app.ui.BaseActivity;
+import com.shunlian.app.utils.Common;
 import com.shunlian.app.utils.MVerticalItemDecoration;
 import com.shunlian.app.utils.PromptDialog;
 import com.shunlian.app.view.IAlipayDetail;
+import com.shunlian.app.widget.MyLinearLayout;
 import com.shunlian.app.widget.MyTextView;
 
 import butterknife.BindView;
@@ -31,8 +41,9 @@ public class AlipayDetailAct extends BaseActivity implements View.OnClickListene
     private PAlipayDetail pAlipayDetail;
     private PromptDialog promptDialog;
 
-    public static void startAct(Context context) {
+    public static void startAct(Context context,boolean hintFlag) {
         Intent intent = new Intent(context, AlipayDetailAct.class);
+        intent.putExtra("hintFlag", hintFlag);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
@@ -73,11 +84,33 @@ public class AlipayDetailAct extends BaseActivity implements View.OnClickListene
         super.initListener();
         mtv_jiechu.setOnClickListener(this);
     }
+    public void initDialog() {
+        Dialog dialog_hint = new Dialog(baseAct, R.style.popAd);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View viewDialog = inflater.inflate(R.layout.dialog_hint_zhi, null);
+        Display display = this.getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
+//设置dialog的宽高为屏幕的宽高
+        ViewGroup.LayoutParams layoutParams = new  ViewGroup.LayoutParams(width, height);
+        dialog_hint.setContentView(viewDialog, layoutParams);
 
+//        dialog_hint.setContentView(R.layout.dialog_hint_zhi);
+        MyTextView mtv_sure = (MyTextView) dialog_hint.findViewById(R.id.mtv_sure);
+        mtv_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog_hint.dismiss();
+            }
+        });
+        dialog_hint.show();
+    }
     @Override
     protected void initData() {
         setStatusBarColor(R.color.white);
         setStatusBarFontDark();
+        if (getIntent().getBooleanExtra("hintFlag", false))
+            initDialog();
         pAlipayDetail = new PAlipayDetail(this, this);
     }
 
--- app/src/main/java/com/shunlian/app/ui/balance/BalanceTXAct.java
+++ app/src/main/java/com/shunlian/app/ui/balance/BalanceTXAct.java
@@ -142,7 +142,8 @@ public class BalanceTXAct extends BaseActivity implements View.OnClickListener, 
     @Override
     public void tiXianCallback(CommonEntity data, int code, String message) {
         if (data != null) {
-            BalanceResultAct.startAct(this, data.amount, data.account, accountType, "");
+            BalanceResultAct.startAct(this, data.amount, data.account,data.account_type,
+                    data.error,data.dialog_text);
         } else {
             String right, left;
             gpv_customUi.clearPassword();
