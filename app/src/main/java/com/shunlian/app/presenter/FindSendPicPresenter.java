73f5c36da00f0079bb3f957843387be881c09b1e
zhanghe <zhanghe@shunliandongli.com>
2018-11-01 16:57:08 +0800

    发布内容数量动态显示，关联商品选择商品数量提示

--- app/src/main/java/com/shunlian/app/presenter/FindSendPicPresenter.java
+++ app/src/main/java/com/shunlian/app/presenter/FindSendPicPresenter.java
@@ -1,15 +1,21 @@
 package com.shunlian.app.presenter;
 
+import android.app.Activity;
 import android.content.Context;
 
+import com.shunlian.app.adapter.SingleImgAdapterV2;
 import com.shunlian.app.bean.BaseEntity;
 import com.shunlian.app.bean.BlogDraftEntity;
 import com.shunlian.app.bean.CommonEntity;
 import com.shunlian.app.bean.UploadPicEntity;
 import com.shunlian.app.listener.SimpleNetDataCallback;
 import com.shunlian.app.photopick.ImageVideo;
+import com.shunlian.app.ui.find_send.BrowseImageVideoAct;
 import com.shunlian.app.utils.Common;
+import com.shunlian.app.utils.LogUtil;
 import com.shunlian.app.view.ISelectPicVideoView;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -21,6 +27,8 @@ import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
+import top.zibin.luban.Luban;
+import top.zibin.luban.OnCompressListener;
 
 /**
  * Created by zhanghe on 2018/10/22.
@@ -32,9 +40,13 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
     private Call<BaseEntity<UploadPicEntity>> uploadMultiPicsCall;
     private Call<BaseEntity<UploadPicEntity>> uploadPicCall;
     private Call<BaseEntity<CommonEntity>> uploadVideoCall;
+    private ArrayList<ImageVideo> mImgList = new ArrayList();
+    private SingleImgAdapterV2 mImgAdapter;
+    private int index = 0;//递归压缩图片下标
 
     public FindSendPicPresenter(Context context, ISelectPicVideoView iView) {
         super(context, iView);
+        attachView();
     }
 
     /**
@@ -42,7 +54,23 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
      */
     @Override
     public void attachView() {
-
+        SingleImgAdapterV2.BuildConfig config = new SingleImgAdapterV2.BuildConfig();
+        config.max_count = 9;
+        config.pictureAndVideo = false;
+        mImgAdapter = new SingleImgAdapterV2(context, mImgList, config);
+        if (iView != null)iView.setAdapter(mImgAdapter);
+        mImgAdapter.setOnItemClickListener((view, position) -> {
+            if (position >= mImgList.size()) {
+                mImgAdapter.selectPic();
+            } else {
+                EventBus.getDefault().postSticky(mImgList);
+                BrowseImageVideoAct.BuildConfig config1 = new BrowseImageVideoAct.BuildConfig();
+                config1.position = position;
+                config1.isShowImageVideo = true;
+                config1.isOnlyBrowse = true;
+                BrowseImageVideoAct.startAct((Activity) context, config1, BrowseImageVideoAct.REQUEST_CODE);
+            }
+        });
     }
 
     /**
@@ -70,7 +98,16 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
         }
         uploadVideoCall = null;
 
+        if (mImgAdapter != null){
+            mImgAdapter.destory();
+            mImgAdapter.unbind();
+            mImgAdapter = null;
+        }
 
+        if (mImgList != null){
+            mImgList.clear();
+            mImgList = null;
+        }
     }
 
     /**
@@ -86,7 +123,23 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
             @Override
             public void onSuccess(BaseEntity<BlogDraftEntity> entity) {
                 super.onSuccess(entity);
-                iView.resetDraft(entity.data);
+                BlogDraftEntity data = entity.data;
+                iView.resetDraft(data);
+
+                if (!isEmpty(data.video)) {
+                    ImageVideo imageVideo = new ImageVideo();
+                    imageVideo.path = data.video;
+                    imageVideo.coverPath = data.video_thumb;
+                    mImgList.add(imageVideo);
+                    mImgAdapter.notifyDataSetChanged();
+                } else if (!isEmpty(data.pics)) {
+                    for (String url : data.pics) {
+                        ImageVideo imageVideo = new ImageVideo();
+                        imageVideo.path = url;
+                        mImgList.add(imageVideo);
+                    }
+                    mImgAdapter.notifyDataSetChanged();
+                }
             }
         });
 
@@ -123,7 +176,11 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
                 super.onSuccess(entity);
                 UploadPicEntity uploadPicEntity = entity.data;
                 if (uploadPicEntity != null) {
-                    iView.uploadImg(uploadPicEntity);
+                    for (int i = 0; i < uploadPicEntity.relativePath.size(); i++) {
+                        mImgList.get(i).url = uploadPicEntity.relativePath.get(i);
+                    }
+                    if (mImgAdapter != null)
+                        mImgAdapter.notifyDataSetChanged();
                 }
             }
         });
@@ -175,6 +232,13 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
                 super.onSuccess(entity);
                 String url = entity.data.name;
                 iView.uploadViodeSuccess(url, videoPath);
+                if (mImgAdapter != null) {
+                    ImageVideo imageVideo = new ImageVideo();
+                    imageVideo.path = videoPath;
+                    imageVideo.url = url;
+                    mImgList.add(imageVideo);
+                    mImgAdapter.notifyDataSetChanged();
+                }
             }
         });
     }
@@ -222,4 +286,76 @@ public class FindSendPicPresenter extends BasePresenter<ISelectPicVideoView> {
     }
 
 
+
+
+    /***
+     * 压缩图片
+     */
+    public void reducePics(List<String> list){
+        if (isEmpty(list))return;
+        index = 0;
+        compress(index,list);
+    }
+
+    /**
+     * 递归压缩图片
+     * @param post
+     * @param list
+     */
+    private void compress(int post,List<String> list){
+        Luban.with(context).load(list.get(post)).putGear(3).setCompressListener(new OnCompressListener() {
+            @Override
+            public void onStart() {
+                LogUtil.httpLogW("递归  开始压缩图片"+list);
+            }
+            @Override
+            public void onSuccess(File file) {
+                ImageVideo imageEntity = new ImageVideo();
+                imageEntity.path = list.get(index);
+                imageEntity.file = file;
+                mImgList.add(imageEntity);
+                index++;
+                if (index < list.size()){
+                    compress(index,list);
+                }else {
+                    uploadPic(mImgList, "find_send");//上传图片
+                }
+            }
+            @Override
+            public void onError(Throwable e) {
+                Common.staticToast("上传图片失败");
+            }
+        }).launch();
+    }
+
+    public String getpicsPath() {
+        if (!isEmpty(mImgList)) {
+            StringBuilder sb = new StringBuilder();
+            for (ImageVideo e : mImgList) {
+                if (!isMP4Path(e.url)) {
+                    sb.append(e.url);
+                    sb.append(",");
+                }
+            }
+            if (sb.length() > 1) {
+                return sb.toString().substring(0, sb.length() - 1);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * 判断是否是MP4文件路径
+     *
+     * @param path
+     * @return
+     */
+    private boolean isMP4Path(String path) {
+        if (isEmpty(path)) return false;
+        else return path.toLowerCase().endsWith(".mp4");
+    }
+
+    public ArrayList<ImageVideo> getmImgList() {
+        return mImgList;
+    }
 }
