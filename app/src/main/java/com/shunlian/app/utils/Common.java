322212ad5ac9ab9eacce3bb462e3e0b498e81ee9
虞班进 <346031156@qq.com>
2018-12-05 13:09:45 +0800

    金蛋修改动画

--- app/src/main/java/com/shunlian/app/utils/Common.java
+++ app/src/main/java/com/shunlian/app/utils/Common.java
@@ -22,6 +22,9 @@ package com.shunlian.app.utils;
 //         .............................................
 //                佛祖保佑                 永无BUG
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.ClipboardManager;
@@ -126,7 +129,7 @@ import java.util.regex.Pattern;
 
 public class Common {
     private static Toast toast, toasts;
-    private static MyTextView mtv_toast, mtv_toasts, mtv_desc;
+    private static MyTextView mtv_toast, mtv_toasts, mtv_desc,ex_desc;
     private static SpannableStringBuilder ssb;
     private static MyImageView miv_logo;
 
@@ -704,6 +707,9 @@ public class Common {
         return matches;
     }
 
+    private static ExToast exToast;
+    private static LottieAnimationView ex_animation_view;
+
     private static Toast toastAnim;
     private static LottieAnimationView animation_view;
 
@@ -751,6 +757,52 @@ public class Common {
             e.printStackTrace();
         }
     }
+
+    public static void staticAnimNewToast(String content,String desc,String jsonAnim) {
+        if (TextUtils.isEmpty(content))
+            return;
+        if (exToast == null) {
+            View v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.toast_anim_new_json, null);
+            ex_desc=  v.findViewById(R.id.mtv_desc);
+            ex_animation_view=  v.findViewById(R.id.animation_view);
+            ex_desc.setText(content);
+            exToast =  ExToast.makeText(getApplicationContext(),"",3);
+            exToast.setView(v);
+            exToast.setAnimations(R.style.ClickToast);
+            exToast.setGravity(Gravity.FILL, 0, 0);
+        } else {
+            ex_desc.setText(desc);
+        }
+        exToast.getView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(ex_animation_view, "alpha", 0f, 1f);
+        objectAnimator.setDuration(300);
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                showAnimExJsonFile(jsonAnim,"images/img_11.png");
+            }});
+        exToast.show();
+        objectAnimator.start();
+    }
+
+
+    private static void showAnimExJsonFile(String jsonAnim,String defaultImage){
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ex_animation_view.setAnimation(jsonAnim);//在assets目录下的动画json文件名。
+                ex_animation_view.loop(false);//设置动画循环播放
+                ex_animation_view.setImageAssetsFolder("images/");//assets目录下的子目录，存放动画所需的图片
+                ex_animation_view.playAnimation();//播放动画
+            } else {
+                AssetManager assets = getApplicationContext().getAssets();
+                ex_animation_view.setImageBitmap(BitmapFactory.decodeStream(assets.open(defaultImage)));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     public static void staticToast(String content) {
         if (TextUtils.isEmpty(content))
