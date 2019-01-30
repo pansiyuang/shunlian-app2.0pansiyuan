2f751cab802593a58f1b73f5a67150702030e5d5
yangxiaofeng <yangxiaofeng@shunliandongli.com>
2019-01-30 17:01:55 +0800

    客服中心优化

--- app/src/main/java/com/shunlian/app/ui/help/HelpSolutionAct.java
+++ app/src/main/java/com/shunlian/app/ui/help/HelpSolutionAct.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -27,6 +28,7 @@ import com.shunlian.app.presenter.PHelpSolution;
 import com.shunlian.app.ui.BaseActivity;
 import com.shunlian.app.utils.Common;
 import com.shunlian.app.utils.Constant;
+import com.shunlian.app.utils.LogUtil;
 import com.shunlian.app.utils.MVerticalItemDecoration;
 import com.shunlian.app.utils.PromptDialog;
 import com.shunlian.app.utils.QuickActions;
@@ -35,7 +37,12 @@ import com.shunlian.app.widget.MyImageView;
 import com.shunlian.app.widget.MyLinearLayout;
 import com.shunlian.app.widget.MyTextView;
 import com.shunlian.app.widget.X5WebView;
+import com.tencent.smtt.export.external.interfaces.SslError;
+import com.tencent.smtt.export.external.interfaces.SslErrorHandler;
+import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
 import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -249,6 +256,7 @@ public class HelpSolutionAct extends BaseActivity implements View.OnClickListene
             mwv_h5.getSettings().setDatabasePath(this.getDir("databases", 0).getPath());
             mwv_h5.getSettings().setGeolocationDatabasePath(this.getDir("geolocation", 0)
                     .getPath());
+//            mwv_h5.setWebViewClient(new WebViewClient());
             // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
             mwv_h5.getSettings().setPluginState(WebSettings.PluginState.ON_DEMAND);
             String contentHtml = "<header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'></header>"+
